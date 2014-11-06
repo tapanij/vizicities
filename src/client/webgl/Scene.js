e148cbf28d2f4d27fff705ca764c19b39dacc2ae
Tapani J. <tapani.jamsa@gmail.com>
2014-11-06 17:51:46 +0200

    Add context broker

--- src/client/webgl/Scene.js
+++ src/client/webgl/Scene.js
@@ -17,6 +17,9 @@ var proj;
 var pois = [];
 var dialogs = [];
 
+var cb_xhr = null; // http request
+var BACKEND_ADDRESS_CB = "http://orion.lab.fi-ware.org:1026/ngsi10/contextEntities/";
+
 // Calculate and set dialog position
 function setDialogPosition(i) {
 	if (dialogs[i] === undefined) {
@@ -100,10 +103,52 @@ function updateDialogs(event) {
 		document.addEventListener('mouseup', this.onDocumentMouseUp, false);
 		document.addEventListener('mousewheel', updateDialogs, false);
 		this.createBox(0, 0, "test name", "test description", 398983);
+
+
+		this.getCBInfo();
 		
 
 	};
 
+	VIZI.Scene.prototype.getCBInfo = function() {
+		function parseCBData(json) {
+		}
+		
+		console.log("Doing search from " + BACKEND_ADDRESS_CB);
+		var restQueryURL = BACKEND_ADDRESS_CB + "urn:smartsantander:testbed:357";
+		console.log("restQueryURL: " + restQueryURL);
+		cb_xhr = new XMLHttpRequest();
+
+		cb_xhr.onreadystatechange = function() {
+			if (cb_xhr.readyState === 4) {
+				if (cb_xhr.status === 200) {
+					console.log("success: " + cb_xhr.responseText);
+					var json = JSON.parse(cb_xhr.responseText);
+					parseCBData(json);
+					console.log(json);
+					var boxLongitude = json.contextElement.attributes[4].value;
+					var boxLatitude = json.contextElement.attributes[3].value;
+					var boxName = "some thing";
+					var boxDescription = "description";
+					var boxId = "9999";
+					city.webgl.scene.createBox(boxLongitude, boxLatitude, boxName, boxDescription, boxId);
+				} else if (cb_xhr.status === 404) {
+					console.log("failed: " + cb_xhr.responseText);
+				}
+				debugger;
+			}
+		}
+
+		cb_xhr.onerror = function(e) {
+			console.log("failed to get CB info.");
+		};
+
+		cb_xhr.open("GET", restQueryURL, true);
+		cb_xhr.setRequestHeader("Content-Type", "application/json");
+		cb_xhr.setRequestHeader("Accept", "application/json");
+		cb_xhr.setRequestHeader("X-Auth-Token", "4wUdbVliV55X5zI68DfDZgVI-by2MBR0s3QhJF7WwwOU0u5AO3f85ycMouzxr3UWGfbCjO3ODcaM6ybtHLcJPA");
+		cb_xhr.send();
+	}
 
 	VIZI.Scene.prototype.makeTextSprite = function(message, parameters) {
 		function roundRect(ctx, x, y, w, h, r) {
