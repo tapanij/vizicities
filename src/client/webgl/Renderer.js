5926f0bfd1b9cb856f9bed75fc31b6a8c39ce9c4
Tapani J. <tapani.jamsa@gmail.com>
2014-11-14 13:53:50 +0200

    backup

--- src/client/webgl/Renderer.js
+++ src/client/webgl/Renderer.js
@@ -2,6 +2,8 @@
 (function() {
 	"use strict";
 
+	var postprocessing = {};
+
 	VIZI.Renderer = function(scene, camera, domContainer) {
 		VIZI.Log("Inititialising WebGL renderer");
 
@@ -37,6 +39,12 @@
 
 		this.domContainer.appendChild(renderer.domElement);
 
+		// DOF
+		debugger;
+		this.initPostprocessing();
+
+
+
 		return renderer;
 	};
 
@@ -50,4 +58,28 @@
 	VIZI.Renderer.prototype.resize = function() {
 		this.renderer.setSize( window.innerWidth, window.innerHeight );
 	};
+
+	VIZI.Renderer.prototype.initPostprocessing = function() {
+				var renderPass = new THREE.RenderPass( this.scene, this.camera );
+
+				var bokehPass = new THREE.BokehPass( this.scene, this.camera, {
+					focus: 		1.0,
+					aperture:	0.025,
+					maxblur:	1.0,
+
+					width: window.innerWidth,
+					height: window.innerHeight
+				} );
+
+				bokehPass.renderToScreen = true;
+
+				var composer = new THREE.EffectComposer( this.renderer );
+
+				composer.addPass( renderPass );
+				composer.addPass( bokehPass );
+
+				postprocessing.composer = composer;
+				postprocessing.bokeh = bokehPass;
+
+			};
 }());
